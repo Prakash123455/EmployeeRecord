63bb027fefb97c520866ae29e05a8dfd09dc1953
prakash kumar <25371560+Prakash123455@users.noreply.github.com>
2021-10-03 10:58:23 -0700

    Update WelcomeController.java

--- src/main/java/com/jwt/api/controller/WelcomeController.java
+++ src/main/java/com/jwt/api/controller/WelcomeController.java
@@ -13,7 +13,7 @@ import com.jwt.api.entity.AuthRequest;
 import com.jwt.api.util.JwtUtil;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
+//@CrossOrigin(origins = "http://localhost:4200")
 public class WelcomeController {
 
     @Autowired
